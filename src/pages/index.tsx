bf023aafb39b58ecde743388325f4aa3b7473136
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-11-19 21:45:44 -0300

    implementa a home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { GetStaticProps } from 'next';
+import Image from 'next/image';
 
 import { format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -30,9 +31,86 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
     <div className={styles.homePage}>
       <section className={styles.latestEpisodes}>
         <h2>Últimos lançamentos</h2>
+
+        <ul>
+            {latestEpisodes.map(episode => {
+              return (
+                <li key={episode.id}>
+
+                  <Image 
+                    width={192} 
+                    height={192} 
+                    src={episode.thumbnail}
+                    alt={episode.title} 
+                    objectFit="cover"
+                  />
+
+                  <div className={styles.episodeDetails}> 
+
+                    <a href="">{episode.title}</a>
+                    <p>{episode.members}</p>
+                    <span>{episode.publishedAt}</span>
+                    <span>{episode.durationAsString}</span>
+
+                  </div>
+
+                  <button type="button">
+                    <img src="/play-green.svg" alt="Tocar episódio" />
+                  </button>
+                </li>
+             )
+           })}
+           
+
+        </ul>
+
       </section>
 
-      <section className={styles.allEpisodes}> 
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+
+        <table cellSpacing={0}>
+
+          <thead>
+            <th></th>
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th></th>
+          </thead>
+
+          <tbody>
+            {allEpisodes.map(episode => {
+              return(
+                <tr key={episode.id}>
+                  <td>
+                    <Image 
+                      width={120}
+                      height={120}
+                      src={episode.thumbnail}
+                      alt={episode.title}
+                      objectFit="cover"
+                    />
+                  </td>
+                  <td>
+                    <a href="">{episode.title}</a>
+                  </td>
+                  <td>{episode.members}</td>
+                  <td style={{ width: 100}}>{episode.publishedAt}</td>
+                  <td>{episode.durationAsString}</td>
+                  <td>
+                    <button type="button">
+                      <img src="/play-green.svg" alt="Tocar episódio" />
+                    </button>
+                  </td>
+                  
+                </tr>
+              )
+            })}
+          </tbody>
+
+        </table>
 
       </section>
     </div>
@@ -78,4 +156,4 @@ export const getStaticProps : GetStaticProps = async () => {
   };
 
 }
-// 31:59
+// 51 ep3
