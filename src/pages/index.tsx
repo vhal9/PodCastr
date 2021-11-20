49d6a0650176cd416c545636980352a14e6ea71f
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-11-20 16:48:34 -0300

    cria página de informação sobre episódio

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { format, parseISO} from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
@@ -17,7 +18,6 @@ type Episode = {
   thumbnail: string,
   duration: number,
   durationAsString: string,
-  description: string,
   url: string,
 }
 
@@ -46,8 +46,10 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                   />
 
                   <div className={styles.episodeDetails}> 
-
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
+                    
                     <p>{episode.members}</p>
                     <span>{episode.publishedAt}</span>
                     <span>{episode.durationAsString}</span>
@@ -94,7 +96,9 @@ export default function Home({latestEpisodes, allEpisodes}: HomeProps) {
                     />
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                   </td>
                   <td>{episode.members}</td>
                   <td style={{ width: 100}}>{episode.publishedAt}</td>
@@ -137,7 +141,6 @@ export const getStaticProps : GetStaticProps = async () => {
       publishedAt: format(parseISO(episode.published_at),'d MMM yy', { locale: ptBR}),
       duration: Number(episode.file.duration),
       durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
-      description: episode.description,
       url: episode.file.url,
     };
   });
@@ -156,4 +159,3 @@ export const getStaticProps : GetStaticProps = async () => {
   };
 
 }
-// 51 ep3
