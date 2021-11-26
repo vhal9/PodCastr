02676eb26cfb25280b0e2d0fdbafa9a3d381e5f6
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-11-26 20:22:14 -0300

    desabilita os botões do player quando não houver episódio tocando

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -57,19 +57,19 @@ export function Player(episódio) {
                 </div>
 
                 <div className={styles.buttons}>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar" />
                     </button>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/play-previous.svg" alt="Tocar anterior" />
                     </button>
-                    <button type="button" className={styles.playButton}>
+                    <button type="button" className={styles.playButton} disabled={!episode}>
                         <img src="/play.svg" alt="Tocar" />
                     </button>
-                    <button type="button" >
+                    <button type="button" disabled={!episode}>
                         <img src="/play-next.svg" alt="Tocar próxima" />
                     </button>
-                    <button type="button" >
+                    <button type="button" disabled={!episode}>
                         <img src="/repeat.svg" alt="Repetir" />
                     </button>
                 </div>
