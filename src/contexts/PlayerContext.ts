34eb356d64e49eed42b758b991fe45d2275ab86c
Victor Hugo Landin <victor.h.landin@gmail.com>
2021-11-26 21:10:36 -0300

    adiciona função de tocar e pausar áudio

--- src/contexts/PlayerContext.ts
+++ src/contexts/PlayerContext.ts
@@ -11,7 +11,10 @@ type Episode = {
 type PlayerContextData = {
     episodeList: Episode[];
     currentEpisodeIndex: number;
+    isPlaying: boolean; 
     play: (episode: Episode) => void;
+    togglePlay: () => void;
+    setPlayingState: (state: boolean) => void
 };
 
 
